f1b8c2982e7a237c4257a73a84563b9bed277971
Vladimir <shadrov_vladimir@mail.ru>
2023-03-28 20:36:12 +0300

    Setup и Ref

--- src/components/composition/pages/AppSetup.vue
+++ src/components/composition/pages/AppSetup.vue
@@ -1,9 +1,57 @@
 <template>
   <h1 class="theme-title">Setup и Ref</h1>
+  <hr />
+  <br />
+  <h3>Задача: Вывести в шаблон реактивные переменные (назнвание фреймворка и его версию)</h3>
+  <br />
+  <hr />
+  <br />
+
+  <!-- ЗДЕСЬ ПИСАТЬ РЕШЕНИЕ -->
+  <div class="wrapper">
+    <h3>Vue Composition Api</h3>
+    <br />
+    <hr />
+    <br />
+
+    <div class="item">
+      Название: <b> {{ title }} </b>
+    </div>
+    <div class="item">
+      Версия: <b> {{ version }} </b>
+    </div>
+    <br />
+
+    <button class="button">Изменить</button>
+  </div>
 </template>
 
 <script>
-export default {};
+import { ref } from 'vue';
+
+export default {
+  setup() {
+    const title = ref('Vue');
+    const version = ref('3');
+
+    return { title, version };
+  },
+};
 </script>
 
-<style></style>
+<style scoped>
+.wrapper {
+  padding: 18px;
+  border: 1px solid black;
+  border-radius: 16px;
+}
+.item {
+  margin: 6px 0;
+}
+.button {
+  cursor: pointer;
+  padding: 4px 8px;
+  text-transform: uppercase;
+  font-weight: bold;
+}
+</style>
