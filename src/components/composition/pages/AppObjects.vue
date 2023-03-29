ba91fccfb9113cafed53f0db374010aa0febd682
Vladimir <shadrov_vladimir@mail.ru>
2023-03-29 18:34:35 +0300

    Удалил решение

--- src/components/composition/pages/AppObjects.vue
+++ src/components/composition/pages/AppObjects.vue
@@ -22,37 +22,19 @@
     <br />
 
     <div class="item">
-      Название: <b> {{ title }} </b>
+      Название: <b> {{}} </b>
     </div>
     <div class="item">
-      Версия: <b> {{ version }} </b>
+      Версия: <b> {{}} </b>
     </div>
     <br />
 
-    <button @click="changeTitle" class="button">Изменить</button>
+    <button class="button">Изменить</button>
   </div>
 </template>
 
 <script>
-import { ref, reactive, toRefs, isRef, isReactive } from 'vue';
-
-export default {
-  setup() {
-    const title = ref('Vue');
-    const version = ref('3');
-    const frameWork = reactive({
-      title: 'React',
-      version: 18,
-    });
-
-    function changeTitle() {
-      title.value = frameWork.title;
-      version.value = frameWork.version;
-    }
-
-    return { title, version, changeTitle };
-  },
-};
+export default {};
 </script>
 
 <style scoped>
